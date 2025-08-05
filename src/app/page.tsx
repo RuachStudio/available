d3f43f31f696baf6dda6fc0225b57c76b3b5b12f
RuachStudio <shockthesheepstudios@gmail.com>
2025-08-04 19:34:07 -0500

    Updated prisma and more...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import SpeakersSection from "./components/SpeakerSection";
 import MerchSection from "./components/MerchSection";
 import TestimoniesSection from "./components/TestimoniesSection";
 import Footer from "./components/Footer";
-
+// Add at top of page.tsx using this API:
+export const dynamic = 'force-dynamic';
 export default function LandingPage() {
   return (
     <main className="bg-black text-white min-h-screen">
